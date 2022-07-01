3650a39e7be5752ca46a36f2021c945837596521
McReseau <47390078+McReseau@users.noreply.github.com>
2022-07-01 20:29:43 +0100

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -4,7 +4,7 @@ NUMBER=$(($FIRSTNUMBER + $SECONDNUMBER))
 echo "$NUMBER"
 echo "this is complete"
 echo "I had a new line"
-echo "Je suis nouveau ici les gars"
+echo "Je suis nouveau ici les gars waou"
 printenv 
 printenv > variable.txt
 echo "End of my script"
