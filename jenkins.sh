0143af625c1d754678f8d90e64b66a0152b829f0
mcreseau <viannijeu@gmail.com>
2022-07-01 21:47:01 +0100

    modify the last echo line

--- jenkins.sh
+++ jenkins.sh
@@ -7,7 +7,7 @@ echo "Je viens de faire quelques modifications les gars"
 echo "I had a new line"
 echo "ça ne marche pas"
 echo 'je teste une autre fois'
-echo "deteste"
+echo "Oui ça marche réellement"
 printenv 
 printenv > variable.txt
 echo "End of my script"
