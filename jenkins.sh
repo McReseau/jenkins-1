4e007f55efb0c6a2eec922ac8eeb5ebffaecc54f
mcreseau <viannijeu@gmail.com>
2022-07-01 20:42:57 +0100

    modifying something

--- jenkins.sh
+++ jenkins.sh
@@ -3,12 +3,9 @@ echo "this script is from GitHub"
 NUMBER=$(($FIRSTNUMBER + $SECONDNUMBER))
 echo "$NUMBER"
 echo "this is complete"
-<<<<<<< HEAD
 echo "Je viens de faire quelques modifications les gars"
-=======
 echo "I had a new line"
-echo "Je suis nouveau ici les gars waou"
->>>>>>> 3650a39e7be5752ca46a36f2021c945837596521
+echo "ça ne marche pas"
 printenv 
 printenv > variable.txt
 echo "End of my script"
